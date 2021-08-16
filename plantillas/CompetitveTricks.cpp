81b5b5404da06d18f107b2062a3dc6ec58009a7a
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-08-16 16:30:19 -0500

    Add files via upload

---
+++ plantillas/CompetitveTricks.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define ll long long int
+#define dll double
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+using namespace std;
+vector <ll> vll;
+
+ll solve()
+{
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
