3f69814b5810776489ee581c3e5d9e4129a2b975
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-08-17 14:15:06 -0500

    Add files via upload

---
+++ UVaOnlineJudge/11172 - Relational Operator.cpp
@@ -0,0 +1,33 @@
+
+#include <bits/stdc++.h>
+
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define ll long long int
+#define dll double
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+using namespace std;
+vector <ll> vll;
+
+ll solve()
+{
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll n;
+    cin>>n;
+    ll a,b;
+    while(n--)
+    {
+        cin>>a>>b;
+        if(a<b)
+            printf("<\n");
+        else if(a>b)
+            printf(">\n");
+        else printf("=\n");
+    }
+}
