3a81f1d25d564898a1512d09ab5a28c297182ccb
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-09-02 20:05:21 -0500

    Add files via upload

---
+++ CodeForces/AStrangeTable.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define ll long long int
+#define dll double
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+using namespace std;
+vector <ll> vll;
+
+ll solve()
+{
+    ll n,m, k;
+    cin>>n>>m>>k;
+  ll filas= ((k-1)%n)+1;
+  ll columnas=ceil((float)(k)/n);
+  return (filas-1)*m+columnas;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+        cout<<solve()<<endl;
+}
