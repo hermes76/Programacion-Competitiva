52977857ba263acd70be633b9c9ba398479f39d9
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-09-10 19:20:52 -0500

    Add files via upload

---
+++ CodeForces/ATheMiracleandtheSleeper.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define ll long long int
+#define dll double
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+using namespace std;
+vector <ll> vll;
+
+ll solve()
+{
+    ll l,r;
+    cin>>l>>r;
+    if((r/2)+1>=l)
+    return r %((r/2)+1);
+    return r%l;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)cout<<solve()<<endl;
+}
+
