1b2aab11ff37282d0d3a353f20314d62b9f7439d
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-09-16 22:36:08 -0500

    Add files via upload

--- CSES/Sorting and Searching/MissingCoinSum.cpp
+++ CSES/Sorting and Searching/MissingCoinSum.cpp
@@ -1,4 +1,3 @@
-
 #include <bits/stdc++.h>
 
 #define forr(n,m,p) for(int x=n; x<m; x+=p)
@@ -6,47 +5,28 @@
 #define dll double
 #define all(x) x.begin(),x.end()
 #define INF 1e9
-#define MOD 10000007
+#define MOD 1000000007
 using namespace std;
 vector <ll> vll;
-ll maxi(ll a, ll b)
+
+ll solve()
 {
-    if(a>b)
-        return a;
-    return b;
-}
-bool isgood(ll arr[], ll n, ll k,ll maximo)
-{
-    ll suma=0;
-    if(k>maximo)
-        return 0;
-    for(ll x=n-1; x>=0; x--)
-    {
-        if(suma+arr[x]<=k)
-            suma+=arr[x];
-    }
-    if(suma==k)
-        return 1;
-    return 0;
-}
-ll solve(ll arr[], ll n)
-{
-    ll k=0;
-    ll maximo=0;
-    for(ll x=0; x<n; x++)maximo=maxi(maximo,arr[x]);
-    for(ll x=maximo/2; x>=1; x>>=2)
-        while(isgood(arr,n,k+n,maximo))k+=n;
-    k++;
-    return k;
-}
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
     ll n;
     cin>>n;
     ll arr[n];
     for(ll x=0; x<n; x++)cin>>arr[x];
     sort(arr,arr+n);
-    cout<<solve(arr,n);
+    ll target=1;
+    for(ll x=0; x<n && target>=arr[x]; x++)
+    {
+        target+=arr[x];
+    }
+    return target;
 }
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout<<solve();
+}
+
