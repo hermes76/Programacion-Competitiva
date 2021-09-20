bc0689355879a82ef100dadce0d0af8863afb137
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-09-20 12:18:38 -0500

    Add files via upload

--- CSES/Sorting and Searching/CollectingNumbersII.cpp
+++ CSES/Sorting and Searching/CollectingNumbersII.cpp
@@ -1,60 +1,58 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define ll long long int
+#define dll double
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
 using namespace std;
+vector <ll> vll;
 
-bool comp(pair<int,int> a,pair<int, int > b)
+void solve()
 {
-    if(a.first<b.first)
-        return 1;
-    return 0;
-}
+   ll n,m;
+   cin>>n>>m;
+   ll nums[n+1];
+   ll positions[n];
+   ll res=1;
+   for(ll x=1; x<=n; x++)
+   {
+       cin>>nums[x];
+       positions[nums[x]]=x;
+   }
 
+   for(ll x=1; x<n; x++)
+        if(positions[x]>positions[x+1])
+            res++;
+    ll l,r;
+    set<pair<ll,ll>> update_pairs;
+    while(m--)
+    {
+        cin>>l>>r;
+        if(nums[l]+1<=n)
+            update_pairs.insert({nums[l], nums[l]+1});
+        if(nums[l]-1>=1)
+             update_pairs.insert({nums[l]-1,nums[l]});
+        if(nums[r]+1<=n)
+            update_pairs.insert({nums[r], nums[r]+1});
+        if(nums[r]-1>=1)
+             update_pairs.insert({nums[r]-1, nums[r]});
+        for(auto x:update_pairs)
+            res-= positions[x.first]>positions[x.second];
+        swap(nums[l],nums[r]);
+        positions[nums[l]]=l;
+        positions[nums[r]]=r;
+        for(auto x: update_pairs)
+            res+=positions[x.first]>positions[x.second];
+        cout<<res<<"\n";
+        update_pairs.clear();
+    }
+}
 int main()
 {
-    int n,m;
-    cin>>n>>m;
-    pair<int,int> arr[n];
-    int indices[n];
-    for(int x=0; x<n; x++){cin>>arr[x].first; arr[x].second=x; indices[x]=arr[x].first;}
-    sort(arr,arr+n,comp);
-    int solve=1;
-    for(int x=1; x<n; x++)
-    {
-        if(arr[x-1].second> arr[x].second)
-            solve++;
-    }
-    cout<<solve<<endl;
-    int a,b,c;
-    int primero,segundo;
-    for(int x=0; x<m; x++)
-    {
-        cin>>a>>b;
-        cout<<arr[indices[a-1]-1].second<<" -> "<<arr[indices[b-1]-1].second<<endl;
-        segundo=arr[indices[a-1]-1].second;
-        primero=arr[indices[b-1]-1].second;
-
-
-        c=arr[indices[a-1]-1].second;
-        arr[indices[a-1]-1].second=arr[indices[b-1]-1].second;
-        arr[indices[b-1]-1].second=c;
-
-        c=indices[a-1];
-        indices[a-1]=indices[b-1];
-        indices[b-1]=c;
-        cout<<arr[indices[a-1]-1].second<<" -> "<<arr[indices[b-1]-1].second<<endl;
-        if(indices[a-1]-2>=0)
-        if(arr[indices[a-1]-2].second < arr[indices[a-1]-1].second and arr[indices[a-1]-2].second>= segundo)
-        {
-            solve--;
-        }else solve++;
-
-        if(indices[b-1]-2>=0)
-         if(arr[indices[b-1]-2].second < arr[indices[b-1]-1].second or arr[indices[b-1]-2].second>= primero)
-        {
-            solve--;
-        }else solve++;
-        cout<<solve<<endl;
-    }
-
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
 }
+
