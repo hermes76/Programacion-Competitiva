2506c68e8a950f2ebc8cd6491c74fa5397b3fdf9
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-09-26 14:45:08 -0500

    Add files via upload

---
+++ CodeForces/ADigitsSum.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;*/
+
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+using namespace std;
+typedef long long ll;
+typedef double dll;
+
+vector <ll> vll;
+
+ll solve()
+{
+    ll n;
+    cin>>n;
+    return (n/10+(((n%10)==9) ? 1:0));
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t; cin>>t;
+    while(t--)
+        cout<<solve()<<endl;
+}
+
