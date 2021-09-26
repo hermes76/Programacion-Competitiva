b70512ea8b6df347b1d542283fe0997cfbc834ef
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-09-26 16:15:58 -0500

    Add files via upload

---
+++ Fproblems/ChangeInAForeingCurrency.cpp
@@ -0,0 +1,84 @@
+
+#include <bits/stdc++.h>
+// Add any extra import statements you may need here
+
+using namespace std;
+
+// Add any helper functions you may need here
+
+
+bool canGetExactChange(int targetMoney, vector<int>& denominations){
+  // Write your code here
+  sort(denominations.rbegin(), denominations.rend());
+  int auxMoney;
+  for(int x=0; x<denominations.size(); x++)
+  {
+    auxMoney= targetMoney;
+    auxMoney%=denominations[x];
+    for(int y=x+1; y<denominations.size(); y++)
+    {
+      auxMoney%=denominations[y];
+    }
+    if(auxMoney ==0)
+      return 1;
+  }
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// These are the tests we use to determine if the solution is correct.
+// You can add your own at the bottom.
+
+void printString(string& str) {
+  cout << "[\"" << str << "\"]";
+}
+
+int test_case_number = 1;
+
+void check(bool expected, bool output) {
+  bool result = (expected == output);
+  const char* rightTick = u8"\u2713";
+  const char* wrongTick = u8"\u2717";
+  if (result) {
+    cout << rightTick << "Test #" << test_case_number << "\n";
+  }
+  else {
+    cout << wrongTick << "Test #" << test_case_number << ": Expected ";
+    printf("%s", expected ? "true" : "false");
+    cout << " Your output: ";
+    printf("%s", output ? "true" : "false");
+    cout << endl;
+  }
+  test_case_number++;
+}
+
+int main(){
+  // Testcase 1
+  int target_1 = 94;
+  vector<int> arr_1{5, 10, 25, 100, 200};
+  bool expected_1 = false;
+  bool output_1 = canGetExactChange(target_1, arr_1);
+  check(expected_1, output_1);
+
+  // Testcase 2
+  int target_2 = 75;
+  vector<int> arr_2{4, 17, 29};
+  bool expected_2 = true;
+  bool output_2 = canGetExactChange(target_2, arr_2);
+  check(expected_2, output_2);
+
+  // Add your own test cases here
+
+  return 0;
+}
