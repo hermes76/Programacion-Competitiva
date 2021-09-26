9b685d5d8e0afc25e4e83d2fb8d5c8328389b9a2
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-09-25 21:17:07 -0500

    Add files via upload

---
+++ CodeForces/AMochaandMath.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>ordered_set;*/
+
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+using namespace std;
+typedef long long ll;
+typedef double dll;
+
+vector <ll> vll;
+
+ll solve()
+{
+    ll n; cin>>n;
+    ll arr[n];
+    for(ll x=0; x<n; x++)cin>>arr[x];
+    ll res=arr[0];
+    for(ll x=1; x<n; x++)
+        res&=arr[x];
+    return res;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--)
+        cout<<solve()<<endl;
+}
+
