c0dc788a55a288dbf6c098ea1fb5586b54a492dd
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-10-11 20:52:13 -0500

    Add files via upload

---
+++ CodeForces/AComputerGame.cpp
@@ -0,0 +1,57 @@
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    string row1,row2;
+    cin>>row1>>row2;
+    for(ll x=0; x<n; x++)
+    {
+        if(row1[x]=='1' && row2[x]=='1')
+        {
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+    cout<<"YES"<<endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    //t=1;
+    cin>>t;
+    while(t--)
+        solve();
+}
+
