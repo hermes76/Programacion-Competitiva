2a5a0ddebcd55cbdd5eda9a2314a576991b6dbe6
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-10-20 15:44:48 -0500

    Add files via upload

--- CSES/Sorting and Searching/MovieFestivalII.cpp
+++ CSES/Sorting and Searching/MovieFestivalII.cpp
@@ -1,45 +1,78 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
 using namespace std;
 
-struct pelicula
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+
+bool comp(pair<ll,ll>a, pair<ll,ll> b)
 {
-    int x,y;
-};
-bool comp(pelicula a, pelicula b)
+    return a.second<b.second;
+}
+void solve()
 {
-    return a.x<b.x;
+    ll n,k;
+    cin>>n>>k;
+
+    vector<pair<ll,ll>> movies(n);
+
+    for(ll x=0; x<n; x++)
+    {
+        ll a,b;
+        cin>>a>>b;
+        movies[x].first=a;
+        movies[x].second=b;
+    }
+
+    sort(all(movies),comp);
+
+    multiset<ll> end_times;
+    for(ll x=0; x<k; x++)
+        end_times.insert(0);
+    ll ans=0;
+
+    for(ll x=0; x<n; x++)
+    {
+        auto it= end_times.upper_bound(movies[x].first);
+
+        if(it==end_times.begin())
+            continue;
+
+        end_times.erase(--it);
+        end_times.insert(movies[x].second);
+        ans++;
+    }
+    cout<<ans;
 }
 int main()
 {
-    cin.tie(NULL);
     ios::sync_with_stdio(false);
-    vector<pelicula> vect;
-    int n,m;
-    int res=1;
-    cin>>n>>m;
-    pelicula arr[n];
-    for(int x =0; x<n; x++){cin>>arr[x].x; cin>>arr[x].y;}
-    sort(arr,arr+n,comp);
-    pelicula mejor=arr[0];
-    vect.push_back(mejor);
-    int indice=0;
-    for(int x =1; x<n; x++)
-    {
-        if(vect[indice].y<=arr[x].x)
-        {
-            indice++;
-        }
-        if(arr[x].x>mejor.x and arr[x].x<mejor.y and arr[x].y>mejor.x and arr[x].y<=mejor.y)
-        {
-            mejor=arr[x];
-            vect.pop_back();
-            vect.push_back(mejor);
-        }
-        else{
-            if(vect.size()-indice<m){vect.push_back(arr[x]);res++;}
-        }
-    }
-    cout<<res;
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    //cin>>t;
+    while(t--)
+        solve();
 }
