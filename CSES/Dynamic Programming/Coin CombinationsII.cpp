4096cbde7881eda2e042ac55398099708f24a06a
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-10-24 13:27:48 -0500

    Add files via upload

---
+++ CSES/Dynamic Programming/Coin CombinationsII.cpp
@@ -0,0 +1,69 @@
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+
+
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+
+    vll nums(n);
+    for(ll x=0; x<n; x++)cin>>nums[x];
+
+    vector<vll> dp(2,vll(m+1));
+
+    for(ll x=0; x<=m; x++)dp[0][x]=0;
+    for(ll x=0; x<2; x++)
+    dp[x][0]=1;
+    for(ll x=1; x<=n; x++)
+    {
+        for(ll y=0; y<=m; y++)
+        {
+            dp[1][y]=dp[0][y];
+            if(y-nums[x-1]>=0)
+            {
+                dp[1][y]+=dp[1][y-nums[x-1]]%MOD;
+                dp[1][y]%=MOD;
+            }
+        }
+        dp[0]=dp[1];
+    }
+
+    cout<<dp[1][m];
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    //cin>>t;
+    while(t--)
+        solve();
+}
