9e388fa7694761ce320501e8124440d62bc1138c
hermes76 <44623908+hermes76@users.noreply.github.com>
2021-10-24 14:12:47 -0500

    Add files via upload

---
+++ HackerRank/BasicDataTypes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define ll long long int
+#define dll double
+#define all(x) x.begin(),x.end()
+#define INF 1e9
+#define MOD 1000000007
+using namespace std;
+vector <ll> vll;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a;
+    ll b;
+    char c;
+    float d;
+    dll e;
+    cin>>a>>b>>c>>d>>e;
+    printf("%d\n%ld\n%c\n%.3f\n%.9lf",a,b,c,d,e);
+}
