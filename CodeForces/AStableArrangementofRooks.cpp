51225c8fc6cdf9059ffe1c1523589fddbe8f6bea
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-01-29 18:23:01 -0600

    Add files via upload

---
+++ CodeForces/AStableArrangementofRooks.cpp
@@ -0,0 +1,82 @@
+
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1000000000000000000
+#define MOD 1000000007
+
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    ll ver=n;
+    if(n==m and n==1)
+    {
+        cout<<'R'<<endl;
+        return;
+    }
+    if(n%2==1)
+        n++;
+    if(m>n/2)
+    {
+        cout<<-1<<endl;
+        return;
+    }
+    n=ver;
+    ll cont=0;
+    for(ll x=0; x<n; x++)
+    {
+        for(ll y=0; y<n; y++)
+        {
+            if(x==y and cont<m)
+            {
+                cout<<'R';
+                cont++;
+            }
+            else cout<<'.';
+        }
+        cout<<endl;
+        x++;
+        if(x<n)
+        {
+            for(ll y=0; y<n; y++)
+                cout<<'.';
+            cout<<endl;
+        }
+
+    }
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    cin>>t;
+    while(t--)
+        solve();
+}
