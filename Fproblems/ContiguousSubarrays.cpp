a2420f483b38e5730f9324448f0634694d7d6ee2
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-02-20 22:03:57 -0600

    Add files via upload

---
+++ Fproblems/ContiguousSubarrays.cpp
@@ -0,0 +1,99 @@
+
+#include <bits/stdc++.h>
+// Add any extra import statements you may need here
+
+using namespace std;
+
+// Add any helper functions you may need here
+
+
+vector <int> CountSubarrays(vector <int> arr) {
+  // Write your code here
+  int N = arr.size();
+  vector<int> L(N,0),R(N,0);
+
+  set<int> values;
+  set<pair<int,int>,greater<pair<int,int>>> index;
+
+  set<int>:: iterator it;
+  set<pair<int,int>,greater<pair<int,int>>> et;
+  for(int x=0; x<N; x++)
+  {
+    it =values.upper_bound(arr[x]);
+    if(it== values.end())
+      L[x]=x+1;
+    else
+    {
+      et=index.lower_bound({(*it),N});
+    }
+  }
+  return {};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// These are the tests we use to determine if the solution is correct.
+// You can add your own at the bottom.
+void printIntegerVector(vector <int> array) {
+  int size = array.size();
+  cout << "[";
+  for (int i = 0; i < size; i++) {
+    if (i != 0) {
+      cout << ", ";
+    }
+   cout << array[i];
+  }
+  cout << "]";
+}
+
+int test_case_number = 1;
+
+void check(vector <int>& expected, vector <int>& output) {
+  int expected_size = expected.size();
+  int output_size = output.size();
+  bool result = true;
+  if (expected_size != output_size) {
+    result = false;
+  }
+  for (int i = 0; i < min(expected_size, output_size); i++) {
+    result &= (output[i] == expected[i]);
+  }
+  const char* rightTick = u8"\u2713";
+  const char* wrongTick = u8"\u2717";
+  if (result) {
+    cout << rightTick << "Test #" << test_case_number << "\n";
+  }
+  else {
+    cout << wrongTick << "Test #" << test_case_number << ": Expected ";
+    printIntegerVector(expected);
+    cout << " Your output: ";
+    printIntegerVector(output);
+    cout << endl;
+  }
+  test_case_number++;
+}
+
+int main() {
+
+  vector <int> test_1{3, 4, 1, 6, 2};
+  vector <int> expected_1{1, 3, 1, 5, 1};
+  vector <int> output_1 = CountSubarrays(test_1);
+  check(expected_1, output_1);
+
+  vector <int> test_2{2, 4, 7, 1, 5, 3};
+  vector <int> expected_2{1, 2, 6, 1, 3, 1};
+  vector <int> output_2 = CountSubarrays(test_2);
+  check(expected_2, output_2);
+
+  // Add your own test cases here
+
+}
