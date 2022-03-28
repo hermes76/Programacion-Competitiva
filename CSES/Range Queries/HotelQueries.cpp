eb0236de3ae0b81d09a5d6a7622514de59f00eb9
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-03-27 18:56:50 -0600

    Add files via upload

---
+++ CSES/Range Queries/HotelQueries.cpp
@@ -0,0 +1,117 @@
+
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1000000000000000000
+#define MOD 1000000007
+struct SegmentTree
+{
+    vector<ll> tree;
+    ll len;
+     SegmentTree()
+    {
+        len=0;
+    }
+    SegmentTree(ll size_)
+    {
+        len=1;
+        for(; len<size_; len<<=1);
+        tree.resize(2*len,0);
+
+    }
+    void construct(vector<ll> &nums)
+    {
+        for(ll x=0; x<nums.size(); x++)
+            tree[x+len]=nums[x];
+        for(ll x=len-1; x>=1; x--)
+            tree[x]= max(tree[2*x],tree[2*x+1]);
+    }
+    ll best(ll turist)
+    {
+        ll pos=0;
+        ll x=1;
+        while(x*2< tree.size())
+        {
+            if(turist<= tree[2*x])
+            {
+                x*=2;
+            }
+            else if(turist<= tree[2*x+1])
+            {
+                x=x*2+1;
+            }
+            else
+                return 0;
+
+             pos=x;
+        }
+        return pos-len+1;
+    }
+
+    void setter(ll position, ll increment)
+    {
+        position += len;
+        tree[position] = increment;
+        for (position /= 2; position >= 1; position /= 2)
+        {
+            tree[position] = max(tree[2*position],tree[2*position+1]);
+        }
+    }
+};
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+
+    vector<ll> rooms(n);
+    vector<ll> query(m);
+
+    for(ll x=0; x<n; x++)cin>>rooms[x];
+
+    for(ll x=0; x<m; x++)cin>>query[x];
+
+    SegmentTree t(rooms.size());
+
+    t.construct(rooms);
+
+    for(ll x=0; x<m; x++)
+    {
+        ll index= t.best(query[x]);
+       cout<<index<<" ";
+        if(index>0)
+            t.setter(index-1,t.tree[t.len+index-1]-query[x]);
+    }
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    //cin>>t;
+    while(t--)
+        solve();
+}
