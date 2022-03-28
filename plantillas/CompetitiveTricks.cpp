fb6d3f639191d6f5234c9af28538e2b1c9020d69
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-03-28 08:29:51 -0600

    Add files via upload

--- plantillas/CompetitiveTricks.cpp
+++ plantillas/CompetitiveTricks.cpp
@@ -14,6 +14,8 @@ using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_no
 template<typename T>
 using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
 
+//order_of_key (K): Number of items strictly smaller than K.
+//find_by_order(k): Kth element in a Set (counting from zero).
 */
 
 //typedefs
@@ -26,7 +28,51 @@ typedef vector<ll> vll;
 #define all(x) x.begin(),x.end()
 #define INF 1000000000000000000
 #define MOD 1000000007
+struct SegmentTree
+{
+    vector<ll> tree;
+    ll len;
+     SegmentTree()
+    {
+        len=0;
+    }
+    SegmentTree(ll size_)
+    {
+        len=1;
+        for(; len<size_; len<<=1);
+        tree.resize(2*len,0);
 
+    }
+    void construct(vector<ll> &nums)
+    {
+        for(ll x=0; x<nums.size(); x++)
+            tree[x+len]=nums[x];
+        for(ll x=len-1; x>=1; x--)
+            tree[x]= tree[2*x]+tree[2*x+1];
+    }
+    ll query(ll l, ll r)
+    {
+        l += len; r += len;
+        ll s = 0;
+        while (l <= r)
+        {
+            if (l%2 == 1) s += tree[l++];
+            if (r%2 == 0) s += tree[r--];
+            l /= 2; r /= 2;
+        }
+        return s;
+    }
+
+    void add(ll position, ll increment)
+    {
+        position += len;
+        tree[position] += increment;
+        for (position /= 2; position >= 1; position /= 2)
+        {
+            tree[position] = tree[2*position]+tree[2*position+1];
+        }
+    }
+};
 void solve()
 {
 
