fb0aad668c710ff2fe69854112a1625c6b0c70f1
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-07-23 18:08:03 -0500

    Add files via upload

---
+++ CodeForces/C_Line_Empire.cpp
@@ -0,0 +1,120 @@
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+//order_of_key (K): Number of items strictly smaller than K.
+//find_by_order(k): Kth element in a Set (counting from zero).
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1000000000000000000
+#define MOD 1000000007
+struct SegmentTree
+{
+    vector<ll> tree;
+    ll len;
+     SegmentTree()
+    {
+        len=0;
+    }
+    SegmentTree(ll size_)
+    {
+        len=1;
+        for(; len<size_; len<<=1);
+        tree.resize(2*len,0);
+
+    }
+    void construct(vector<ll> &nums)
+    {
+        for(ll x=0; x<nums.size(); x++)
+            tree[x+len]=nums[x];
+        for(ll x=len-1; x>=1; x--)
+            tree[x]= tree[2*x]+tree[2*x+1];
+    }
+    ll query(ll l, ll r)
+    {
+        l += len; r += len;
+        ll s = 0;
+        while (l <= r)
+        {
+            if (l%2 == 1) s += tree[l++];
+            if (r%2 == 0) s += tree[r--];
+            l /= 2; r /= 2;
+        }
+    return s;
+    }
+    void remplace(ll position, ll new_value)
+    {
+        position += len;
+        tree[position] = new_value;
+        for (position /= 2; position >= 1; position /= 2)
+        {
+            tree[position] = tree[2*position]+tree[2*position+1];
+        }
+    }
+};
+void solve()
+{
+    ll n,a,b;
+    cin>>n>>a>>b;
+
+    vll nums(n+1);
+    nums[0]=0;
+    for(ll x=1; x<=n; x++)
+        cin>>nums[x];
+    
+    vll suffix(n+1,0);
+
+    suffix[0]=0;
+    for(ll x=1; x<=n; x++)
+      suffix[x]= suffix[x-1]+nums[x];
+    
+
+    ll best_solution=suffix[n] *b;
+    ll index_a=0;
+    ll left_part=0;
+    for(ll x=1; x<=n; x++)
+    {
+       ll distance= nums[x]-nums[index_a];
+       ll cost= distance*a;
+       left_part += distance*b; 
+       ll right_part = suffix[n]-(nums[x]*(n-x))- suffix[x];
+       right_part*=b;
+       if(cost+left_part+ right_part <= best_solution)
+       {
+            left_part+=cost;
+            best_solution=left_part+right_part;
+            index_a= x;
+       }
+    }
+    cout<< best_solution<<endl;
+
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    cin>>t;
+    for(ll x=0; x<t; x++)
+        solve();
+}
