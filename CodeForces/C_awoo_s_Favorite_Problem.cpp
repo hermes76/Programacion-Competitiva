dae03c8e65e26e2cf2c4fbd3751ff2d4f8c9865b
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-08-03 19:35:03 -0500

    Add files via upload

---
+++ CodeForces/C_awoo_s_Favorite_Problem.cpp
@@ -0,0 +1,129 @@
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+//order_of_key (K): Number of items strictly smaller than K.
+//find_by_order(k): Kth element in a Set (counting from zero).
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1000000000000000000
+#define MOD 1000000007
+struct SegmentTree
+{
+    vector<ll> tree;
+    ll len;
+     SegmentTree()
+    {
+        len=0;
+    }
+    SegmentTree(ll size_)
+    {
+        len=1;
+        for(; len<size_; len<<=1);
+        tree.resize(2*len,0);
+
+    }
+    void construct(vector<ll> &nums)
+    {
+        for(ll x=0; x<nums.size(); x++)
+            tree[x+len]=nums[x];
+        for(ll x=len-1; x>=1; x--)
+            tree[x]= tree[2*x]+tree[2*x+1];
+    }
+    ll query(ll l, ll r)
+    {
+        l += len; r += len;
+        ll s = 0;
+        while (l <= r)
+        {
+            if (l%2 == 1) s += tree[l++];
+            if (r%2 == 0) s += tree[r--];
+            l /= 2; r /= 2;
+        }
+    return s;
+    }
+    void remplace(ll position, ll new_value)
+    {
+        position += len;
+        tree[position] = new_value;
+        for (position /= 2; position >= 1; position /= 2)
+        {
+            tree[position] = tree[2*position]+tree[2*position+1];
+        }
+    }
+}; 
+void solve()
+{
+    ll n;
+    cin>>n;
+    string s,t;
+    cin>>s>>t;
+
+    vll cont(3,0);
+    for(ll x=0; x<n; x++)
+    {
+        cont[s[x]-'a']++;
+        cont[t[x]-'a']--;
+    }
+    if(cont[0]!=0 || cont[1]!=0 || cont[2]!=0)
+    {
+        cout<<"NO"<<endl;
+        return;
+    }
+    ll index=0;
+    for(ll x=0; x<n; x++)
+    {
+        if(s[x]=='b')
+            continue;
+        while(t[index]=='b')
+            index++;
+        if(t[index]!=s[x])
+        {
+            cout<<"NO"<<endl;
+            return;
+        }
+        if(s[x]=='c' && index>x)
+        {
+            cout<<"NO"<<endl;
+            return;
+        }
+        if(s[x]=='a' && index<x)
+        {
+            cout<<"NO"<<endl;
+            return;
+        }
+        index++;
+
+    }
+
+    cout<<"YES"<<endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    cin>>t;
+    for(ll x=0; x<t; x++)
+        solve();
+}
