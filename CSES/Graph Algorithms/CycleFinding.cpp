aea4cbf25a409275caf5f771237f43277071a1d6
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-12-27 14:48:07 -0600

    Add files via upload

---
+++ CSES/Graph Algorithms/CycleFinding.cpp
@@ -0,0 +1,146 @@
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+//order_of_key (K): Number of items strictly smaller than K.
+//find_by_order(k): Kth element in a Set (counting from zero).
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1000000000000000000
+#define MOD 1000000007
+struct SegmentTree
+{
+    vector<ll> tree;
+    ll len;
+     SegmentTree()
+    {
+        len=0;
+    }
+    SegmentTree(ll size_)
+    {
+        len=1;
+        for(; len<size_; len<<=1);
+        tree.resize(2*len,0);
+
+    }
+    void construct(vector<ll> &nums)
+    {
+        for(ll x=0; x<nums.size(); x++)
+            tree[x+len]=nums[x];
+        for(ll x=len-1; x>=1; x--)
+            tree[x]= tree[2*x]+tree[2*x+1];
+    }
+    ll query(ll l, ll r)
+    {
+        l += len; r += len;
+        ll s = 0;
+        while (l <= r)
+        {
+            if (l%2 == 1) s += tree[l++];
+            if (r%2 == 0) s += tree[r--];
+            l /= 2; r /= 2;
+        }
+    return s;
+    }
+    void remplace(ll position, ll new_value)
+    {
+        position += len;
+        tree[position] = new_value;
+        for (position /= 2; position >= 1; position /= 2)
+        {
+            tree[position] = tree[2*position]+tree[2*position+1];
+        }
+    }
+};
+void bellman(vector<tuple<ll,ll,ll>> edges,ll n,ll m)
+{
+    vll distances(n+1,INF);
+    vll ancester(n+1,-1); 
+    distances[1]=0;
+
+    ll index=-1;
+    for(int i= 1; i<=n; i++)
+    {
+        index=-1;
+        for(auto e: edges){
+            int a, b,w;
+            tie(a,b,w) = e;
+            if(distances[b] > distances[a]+w)
+            {
+                distances[b] = min(distances[b], distances[a]+w);
+                index=b;
+                ancester[b]=a;
+            }
+
+        }
+    }
+
+    if(index==-1)
+    {
+        cout<<"NO"<<endl;
+    }
+    else{
+        cout<<"YES"<<endl;
+        for(ll x=0; x<n; x++)
+        {
+            //cout<<index<<' '<<ancester[index]<<endl;
+            index=ancester[index];
+        }
+
+        vll ans;
+        
+        for(ll root=index;; root=ancester[root])
+        {
+            ans.push_back(root);
+            if(ans.size()>1 && root== index)
+                break;
+        }
+        reverse(all(ans));
+        for(auto x: ans)
+            cout<<x<<' ';
+
+    }
+}
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    vector<tuple<ll,ll,ll>>edges;
+    for(ll x=0; x<m; x++)
+    {
+        ll a,b,c;
+        cin>>a>>b>>c;
+        edges.push_back({a,b,c});
+    }
+
+    bellman(edges,n,m);
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    //cin>>t;
+    for(ll x=0; x<t; x++)
+        solve();
+}
