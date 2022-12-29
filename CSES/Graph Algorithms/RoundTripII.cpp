a671e13779673e6ca9bf05b52b81adb642c4aad3
hermes76 <44623908+hermes76@users.noreply.github.com>
2022-12-28 18:38:00 -0600

    Add files via upload

---
+++ CSES/Graph Algorithms/RoundTripII.cpp
@@ -0,0 +1,157 @@
+//libraries
+#include <bits/stdc++.h>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+
+//namespaces
+//using namespace __gnu_pbds;
+using namespace std;
+
+//templates
+/*
+template<typename T>
+using ordered_set= tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>
+using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag, tree_order_statistics_node_update>;
+//order_of_key (K): Number of items strictly smaller than K.
+//find_by_order(k): Kth element in a Set (counting from zero).
+*/
+
+//typedefs
+typedef long long ll;
+typedef double dll;
+typedef vector<ll> vll;
+
+//defines
+#define forr(n,m,p) for(int x=n; x<m; x+=p)
+#define all(x) x.begin(),x.end()
+#define INF 1000000000000000000
+#define MOD 1000000007
+struct SegmentTree
+{
+    vector<ll> tree;
+    ll len;
+     SegmentTree()
+    {
+        len=0;
+    }
+    SegmentTree(ll size_)
+    {
+        len=1;
+        for(; len<size_; len<<=1);
+        tree.resize(2*len,0);
+
+    }
+    void construct(vector<ll> &nums)
+    {
+        for(ll x=0; x<nums.size(); x++)
+            tree[x+len]=nums[x];
+        for(ll x=len-1; x>=1; x--)
+            tree[x]= tree[2*x]+tree[2*x+1];
+    }
+    ll query(ll l, ll r)
+    {
+        l += len; r += len;
+        ll s = 0;
+        while (l <= r)
+        {
+            if (l%2 == 1) s += tree[l++];
+            if (r%2 == 0) s += tree[r--];
+            l /= 2; r /= 2;
+        }
+    return s;
+    }
+    void remplace(ll position, ll new_value)
+    {
+        position += len;
+        tree[position] = new_value;
+        for (position /= 2; position >= 1; position /= 2)
+        {
+            tree[position] = tree[2*position]+tree[2*position+1];
+        }
+    }
+};
+vector<ll> visited_all;
+vector<vll> graph;
+vector<bool> visited;
+vll ans;
+
+void find_cycle(ll root,ll order)
+{
+    if(visited[root])
+    {
+        ans.push_back(root);
+        return;
+    }
+    visited[root]=true;
+    visited_all[root]=order;
+
+    for(ll x=0; x<graph[root].size(); x++)
+    {
+        if(visited_all[graph[root][x]]<order)
+            continue;
+        find_cycle(graph[root][x],order);
+        if(ans.size()>0)
+        {
+            ans.push_back(root);
+            return;
+        }
+    }
+
+    visited[root]=false;
+}
+void solve()
+{
+    ll n,m;
+    cin>>n>>m;
+    graph.resize(n+1);
+    visited.resize(n+1,false);
+    visited_all.resize(n+1,INF);
+
+    while(m--)
+    {
+        ll a,b;
+        cin>>a>>b;
+        graph[a].push_back(b);
+    }
+
+    ll aux=n;
+    ll order=0;
+    while(aux--)
+    {
+        if(visited_all[aux]!=INF)
+            continue;
+        find_cycle(aux,order);
+        order++;
+        if(ans.size()>0)
+            break;
+
+    }
+    if(ans.size()>0)
+    {
+        vll cycle;
+        vector<bool> v(n+1,false);
+        for(ll y=0; y<ans.size() && !v[ans[y]]; y++)
+        {
+            cycle.push_back(ans[y]);
+            v[ans[y]]=true;
+        }
+        cycle.push_back(ans[0]);
+        reverse(all(cycle));
+        cout<<cycle.size()<<endl;
+        for(ll nodes: cycle)
+            cout<<nodes<<' ';
+        return;
+    }
+    cout<<"IMPOSSIBLE";
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    t=1;
+    //cin>>t;
+    for(ll x=0; x<t; x++)
+        solve();
+}
